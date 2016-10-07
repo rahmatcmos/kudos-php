f199f542e56057e8f5cb6bca176148e0c83efbf5
Trevor Sewell <trev@kudos.agency>
2016-10-07 09:31:01 +0100

    update main gulp file for admin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const elixir = require('laravel-elixir');
 
-require('laravel-elixir-vue');
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -13,7 +11,28 @@ require('laravel-elixir-vue');
  |
  */
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+elixir(function(mix) {
+  mix.sass('./resources/assets/admin/sass/app.scss', 'resources/assets/admin/css/modules');
+    
+  mix.styles([
+    './node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './node_modules/font-awesome/css/font-awesome.min.css',
+    './node_modules/summernote/dist/summernote.css',
+    './node_modules/dropzone/dist/dropzone.css',
+    './node_modules/swipebox/src/css/swipebox.min.css',
+    './resources/assets/admin/css/third-party/*.css', 
+    './resources/assets/admin/css/modules/*.css'
+  ], './public/build/admin/css');
+  mix.scripts([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/dropzone/dist/dropzone.js',
+    './node_modules/summernote/dist/summernote.js',
+    './node_modules/chart.js/dist/Chart.min.js',
+    './resources/assets/admin/js/third-party/*.js',
+    './node_modules/nestedSortable/jquery.mjs.nestedSortable.js',
+    './node_modules/swipebox/src/js/jquery.swipebox.min.js',
+    './resources/assets/admin/js/modules/*.js'
+  ], './public/build/admin/js');
+    
 });
